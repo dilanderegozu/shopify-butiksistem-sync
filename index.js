ab3c51a5593515b0e8350b48c644c6481949ecaf
Dilan Deregözü <dilanderegizu@icloud.com>
2026-06-15 14:07:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,83 @@ app.post("/shopify-order", async (req, res) => {
     console.log("SHOPIFY ORDER ID:", req.body.id);
     console.log("ORDER NUMBER:", req.body.order_number);
 
-    ...
+    
+});
+
+app.get("/test-order", async (req, res) => {
+
+    try {
+
+        const result = await axios.post(
+            "https://rema.butiksistem.com/rest/order/add",
+            {
+                auth: {
+                    userName: process.env.BUTIK_USER,
+                    password: process.env.BUTIK_PASS
+                },
+                arguments: {
+                    customOrderId: "SHOPIFY-TEST-1",
+                    orderDate: "2026-06-15",
+                    orderPaymentTypeId: 1,
+                    orderShippingValue: 0,
+                    orderProductsValue: 1200,
+                    orderPaymentConfirmStat: 0,
+                    orderStatusId: 3,
+
+                    delivery: {
+                        name: "Dilan",
+                        surName: "Deregözü",
+                        mail: "dilan.deregizu@gmail.com",
+                        phone: "05010718237",
+                        address: "Test Adres",
+                        city: "İstanbul",
+                        district: "Gaziosmanpaşa",
+                        cargoCompanyId: "11",
+                        cargoCompanyExtArgs: {
+                            company_id: "11",
+                            software_type: "1",
+                            branchCode: "POE",
+                            branchName: "PTT",
+                            gCompanyId: "11"
+                        },
+                        phoneCode: 90
+                    },
+
+                    billing: {
+                        name: "Dilan",
+                        surName: "Deregözü",
+                        mail: "dilan.deregizu@gmail.com",
+                        phone: "05010718237",
+                        address: "Test Adres",
+                        city: "İstanbul",
+                        district: "Gaziosmanpaşa",
+                        phoneCode: 90
+                    },
+
+                    notification: {
+                        sms: true
+                    },
+
+                    items: [
+                        {
+                            variantId: 1846,
+                            quantity: 1
+                        }
+                    ]
+                }
+            }
+        );
+
+        res.json(result.data);
+
+    } catch (err) {
+
+        console.error(err.response?.data || err.message);
+
+        res.status(500).json(
+            err.response?.data || err.message
+        );
+    }
 });
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server çalışıyor");
